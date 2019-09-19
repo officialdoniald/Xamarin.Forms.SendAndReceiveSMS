cd00481b96f662dd5f47b3cca4bc682d779a5d71
Lénárt Bence <bence960206@gmail.com>
2019-09-19 08:13:17 +0200

    iOS Send SMS implementation

---
+++ SendAndReceiveSMS/SendAndReceiveSMS.iOS/Receivers/SendSms.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MessageUI;
+using SendAndReceiveSMS.Interfaces;
+using UIKit;
+
+namespace SendAndReceiveSMS.iOS.Receivers
+{
+    public class SendSms : ISendSms
+    {
+        public void Send(string body, string phoneNumber)
+        {
+            if (!MFMailComposeViewController.CanSendMail)
+                return;
+
+            MFMessageComposeViewController smsController = new MFMessageComposeViewController();
+
+            smsController.Recipients = new[] { phoneNumber };
+            smsController.Body = body;
+
+            EventHandler<MFMessageComposeResultEventArgs> handler = null;
+            handler = (sender, args) =>
+            {
+                smsController.Finished -= handler;
+
+                var uiViewController = sender as UIViewController;
+                if (uiViewController == null)
+                {
+                    throw new ArgumentException("sender");
+                }
+
+                uiViewController.DismissViewControllerAsync(true);
+            };
+
+            smsController.Finished += handler;
+
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewControllerAsync(smsController, true);
+        }
+    }
+}
